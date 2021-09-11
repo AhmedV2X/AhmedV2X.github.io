7d0d825c203ec7624640df7c4490284480638043
AhmedV2X <48530506+AhmedV2X@users.noreply.github.com>
2021-09-12 01:11:00 +0300

    updated

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,64 +1,65 @@
+let tl2 = gsap.timeline();
 
-function runProjects(){
+function runProjects() {
 
     animateMenu();
     sectionOne();
-    
+
 }
-function sectionOne(){
+function sectionOne() {
     gsap.timeline()
 
-    .to("section:nth-of-type(1)", {duration: 1, opacity:1,delay:2})
+        .to("section:nth-of-type(1)", { duration: 1, opacity: 1, delay: 2 })
 }
 
-function animateMenu(){
+function animateMenu() {
     gsap.timeline()
 
-    .to("header", {duration: 0, display:"flex"})
-    .to("header", {duration: 1, width:"100%"})
-    .to("nav", {duration: 1, opacity:1,height:"100%"})
- 
+        .to("header", { duration: 0, display: "flex" })
+        .to("header", { duration: 1, width: "100%" })
+        .to("nav", { duration: 1, opacity: 1, height: "100%" })
+
 }
 //slider code
-var slideCount=1;
-function nextSlide(totalSlides){
+var slideCount = 1;
+function nextSlide(totalSlides) {
 
-    let ele=".panel:nth-of-type("+slideCount+")";
+    let ele = ".panel:nth-of-type(" + slideCount + ")";
 
-    gsap.timeline()
-    .to(ele, {duration: 1, opacity:0})
-    .to(ele, {duration: 0, display:"none"})
+    tl2
+        .to(ele, { duration: 1, opacity: 0 })
+        .to(ele, { duration: 0, display: "none" })
 
     slideCount++;
-    if(slideCount>totalSlides){
-        slideCount=1;
+    if (slideCount > totalSlides) {
+        slideCount = 1;
 
     }
-    ele=".panel:nth-of-type("+slideCount+")";
+    ele = ".panel:nth-of-type(" + slideCount + ")";
 
-    gsap.timeline()
-    .to(ele, {duration: 0, display:"flex"})
-    .to(ele, {duration: 0.7, opacity:1,delay:1})
-    document.getElementById("slideNumber").innerText=slideCount;
+    tl2
+        .to(ele, { duration: 0, display: "flex" })
+        .to(ele, { duration: 0.7, opacity: 1, delay: 1 })
+    document.getElementById("slideNumber").innerText = slideCount;
 }
-function preSlide(totalSlides){
+function preSlide(totalSlides) {
 
-    let ele=".panel:nth-of-type("+slideCount+")";
+    let ele = ".panel:nth-of-type(" + slideCount + ")";
 
-    gsap.timeline()
-    .to(ele, {duration: 1, opacity:0})
-    .to(ele, {duration: 1, display:"none"})
+    tl2
+        .to(ele, { duration: 1, opacity: 0 })
+        .to(ele, { duration: 1, display: "none" })
 
     slideCount--;
-    if(slideCount<1){
-        slideCount=totalSlides;
+    if (slideCount < 1) {
+        slideCount = totalSlides;
 
     }
-    ele=".panel:nth-of-type("+slideCount+")";
+    ele = ".panel:nth-of-type(" + slideCount + ")";
 
-    gsap.timeline()
-    .to(ele, {duration: 1, display:"flex"})
-    .to(ele, {duration: 0.7, opacity:1})
-    document.getElementById("slideNumber").innerText=slideCount;
+    tl2
+        .to(ele, { duration: 1, display: "flex" })
+        .to(ele, { duration: 0.7, opacity: 1 })
+    document.getElementById("slideNumber").innerText = slideCount;
 
 }
